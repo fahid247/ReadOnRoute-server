69aeba2e98dbe1f96ea14a62c6dc8405ef1193b9
Fahid Aqundow <136666497+fahid247@users.noreply.github.com>
2025-12-17 21:57:53 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('reader reading reading...')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

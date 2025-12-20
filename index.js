d05cfaa446b2e19a6e61f788f7de7f305a897789
Fahid Aqundow <136666497+fahid247@users.noreply.github.com>
2025-12-20 13:09:47 +0600

    done till jwt

--- index.js
+++ index.js
@@ -3,12 +3,48 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
+const crypto = require("crypto");
+
+function generateTrackingId() {
+    const prefix = "PRCL"; // your brand prefix
+    const date = new Date().toISOString().slice(0, 10).replace(/-/g, ""); // YYYYMMDD
+    const random = crypto.randomBytes(3).toString("hex").toUpperCase(); // 6-char random hex
+
+    return `${prefix}-${date}-${random}`;
+}
+
+
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
 //middleware
 app.use(express.json())
 app.use(cors())
 
+
+/*const verifyFBToken = async (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    try {
+        const idToken = token.split(' ')[1];
+        const decoded = await admin.auth().verifyIdToken(idToken);
+        console.log('decoded in the token', decoded);
+        req.decoded_email = decoded.email;
+        next();
+    }
+    catch (err) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+
+}*/
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rpcowue.mongodb.net/?appName=Cluster0`;
 
@@ -30,27 +66,157 @@ async function run() {
         const db = client.db('read_On_Route_db');
         const UserCollection = db.collection('users');
         const BooksCollection = db.collection('AllBooks');
-        const OrdersCollection = db.collection('orders')
+        const OrdersCollection = db.collection('orders');
+        const PaymentCollection = db.collection('payments');
+
+
+
+
+
+
+
+
+        //payment related apis
+        app.post('/create-checkout-session', async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = parseInt(paymentInfo.price) * 100;
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+
+                        price_data: {
+                            currency: 'BDT',
+                            unit_amount: amount,
+                            product_data: {
+                                name: paymentInfo.name
+                            }
+                        },
+                        quantity: 1,
+                    },
+                ],
+                mode: 'payment',
+                customer_email: paymentInfo.email,
+                metadata: {
+                    orderId: paymentInfo.orderId,
+                    orderName: paymentInfo.name
+                },
+                success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+            })
+
+            res.send({ url: session.url })
+        })
+
+        const trackingId = generateTrackingId()
+
+        app.patch('/payment-success', async (req, res) => {
+            const sessionId = req.query.session_id;
+            const session = await stripe.checkout.sessions.retrieve(sessionId);
+            //console.log(session)
+
+            const transactionId = session.payment_intent;
+            const query = { transactionId: transactionId }
+
+            const paymentExist = await paymentCollection.findOne(query);
+            console.log(paymentExist);
+            if (paymentExist) {
+
+                return res.send({
+                    message: 'already exists',
+                    transactionId,
+                    trackingId: paymentExist.trackingId
+                })
+            }
+
+
+
+            if (session.payment_status === 'paid') {
+                const id = session.metadata.orderId;
+                const query = { _id: new ObjectId(id) }
+                const update = {
+                    $set: {
+                        paymentStatus: 'paid',
+                        trackingId: trackingId
+
+                    }
+                }
+
+                const result = await OrdersCollection.updateOne(query, update);
+
+                const payment = {
+                    amount: session.amount_total / 100,
+                    currency: session.currency,
+                    customerEmail: session.customer_email,
+                    orderId: session.metadata.orderId,
+                    orderName: session.metadata.orderName,
+                    transactionId: session.payment_intent,
+                    paymentStatus: session.payment_status,
+                    paidAt: new Date(),
+                }
+
+
+                if (session.payment_status === 'paid') {
+                    const resultPayment = await PaymentCollection.insertOne(payment)
+                    res.send({
+                        success: true,
+                        modifyOrder: result,
+                        trackingId: trackingId,
+                        paymentInfo: resultPayment
+                    })
+                }
+
+
+            }
+            res.send({ success: true })
+
+        })
+
+        app.get('/payments', async (req, res) => {
+            const email = req.body;
+            const query = {}
+
+            if (email) {
+                query.customerEmail = email;
+
+                /*if (email !== req.decoded_email) {
+                    return res.status(403).send({ message: 'forbidden access' })
+                }*/
+            }
+            const cursor = PaymentCollection.find(query).sort({ paidAt: -1 });
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
 
         //orders api
-        app.post('/orders',async(req,res)=>{
+        app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await OrdersCollection.insertOne(order);
             res.send(result)
         })
 
-        app.get('/orders',async(req,res)=>{
+        app.get('/orders', async (req, res) => {
             const cursor = OrdersCollection.find();
             const result = await cursor.toArray();
             res.send(result)
         })
 
-
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const { orderStatus } = req.body;
+            const query = { _id: new ObjectId(id) }
+            const update = {
+                $set: {
+                    orderStatus: orderStatus
+                }
+            }
+            const result = await OrdersCollection.updateOne(query, update)
+            res.send(result)
+        })
 
         app.get('/orders', async (req, res) => {
             const query = {}
-            const { email} = req.query;
+            const { email } = req.query;
 
             if (email) {
                 query.email = email;
@@ -60,6 +226,14 @@ async function run() {
 
             const cursor = OrdersCollection.find(query, options);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await OrdersCollection.findOne(query);
             res.send(result);
         })
 

316eb362e8aa0e0f9a076efc70f28601e011175c
Fahid Aqundow <136666497+fahid247@users.noreply.github.com>
2025-12-22 18:25:53 +0600

    done with all

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const app = express()
 const port = process.env.PORT || 3000
 const crypto = require("crypto");
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
+
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./read-on-route-firebase-adminsdk.json");
+// const serviceAccount = require("./read-on-route-firebase-adminsdk.json");
+
+
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -25,9 +33,8 @@ function generateTrackingId() {
 
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET)
+
 
 //middleware
 app.use(express.json())
@@ -70,7 +77,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('read_On_Route_db');
         const UserCollection = db.collection('users');
@@ -428,8 +435,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         //await client.close();
